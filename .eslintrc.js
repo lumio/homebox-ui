e8b399673de5547c682e00fa0d9d6fa82cf0dc80
Benjamin Falk <lumio@lumio.at>
2023-12-26 13:26:10 +0100

    enforce rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,49 @@ module.exports = {
   plugins: [
     "@typescript-eslint",
     "@stylistic/ts",
+    // "@stylistic/tsx",
   ],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
-    "eslint:recommended"
+    "eslint:recommended",
+    "plugin:@stylistic/all-extends",
+    "prettier",
   ],
   root: true,
   rules: {
-    "@stylistic/ts/indent": ["error", 2],
-    "indent": [
-      "error",
-      2
+    "@stylistic/indent": ["error", 2],
+    "@stylistic/array-element-newline": [
+      "error", {
+        ArrayExpression: "consistent",
+        ArrayPattern: {
+          minItems: 3,
+        },
+      },
     ],
+    "@stylistic/comma-dangle": ["error", "always-multiline"],
+    "@stylistic/comma-spacing": "error",
+    "@stylistic/function-call-spacing": "error",
+    "@stylistic/quote-props": ["error", "as-needed"],
+    "@stylistic/space-before-function-paren": [
+      "error", {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    "@stylistic/padded-blocks": ["error", "never"],
+    "@stylistic/spaced-comment": ["error", "always", {markers: ["/"]}],
+
+    "eol-last": ["error", "always"],
     "linebreak-style": [
       "error",
-      "unix"
+      "unix",
     ],
-    "quotes": [
+    quotes: [
       "error",
-      "double"
+      "double",
     ],
-  }
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+  },
 };
